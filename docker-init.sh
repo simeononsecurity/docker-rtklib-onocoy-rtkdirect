df8a6370cef8786e56b6319680a0db7fe42abc71
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2023-11-26 19:04:30 -0600

    Update docker-init.sh

--- docker-init.sh
+++ docker-init.sh
@@ -13,22 +13,19 @@ export STOP_BITS="${STOP_BITS:-1}"
 # Construct SERIAL_INPUT using individual components
 export SERIAL_INPUT="serial://$USB_PORT:$BAUD_RATE:$DATA_BITS:$PARITY:$STOP_BITS#rtcm3"
 
-# Set RTCM_MSG_COMMON without latitude, longitude, and elevation
-export RTCM_MSG_COMMON="-msg \"1006(30), 1033(30), 1077, 1087, 1097, 1107, 1117, 1127, 1137, 1230\" -t 0"
-
 # Check if LAT, LONG, and ELEVATION are specified
 if [ -n "$LAT" ] && [ -n "$LONG" ] && [ -n "$ELEVATION" ]; then
-    RTCM_MSG_COMMON="-p \"$LAT $LONG $ELEVATION\" $RTCM_MSG_COMMON"
+    LAT_LONG_ELEVATION="-p \"$LAT $LONG $ELEVATION\""
 fi
 
 # Check if INSTRUMENT is specified
 if [ -n "$INSTRUMENT" ]; then
-    RTCM_MSG_COMMON+=" -i \"$INSTRUMENT\""
+    INSTRUMENT="-i \"$INSTRUMENT\""
 fi
 
 # Check if ANTENNA is specified
 if [ -n "$ANTENNA" ]; then
-    RTCM_MSG_COMMON+=" -a \"$ANTENNA\""
+    ANTENNA="-a \"$ANTENNA\""
 fi
 
 # Exit immediately if a command fails
@@ -41,12 +38,12 @@ fi
 
 # Run the second command only if all required parameters are specified
 if [ -n "$PASSWORD" ] && [ -n "$ONOCOY_USERNAME" ]; then
-    str2str -in tcpcli://127.0.0.1:5015#rtcm3 -out ntrips://:$PASSWORD@servers.onocoy.com:2101/$ONOCOY_USERNAME#rtcm3 $RTCM_MSG_COMMON &
+    str2str -in tcpcli://127.0.0.1:5015#rtcm3 -out ntrips://:$PASSWORD@servers.onocoy.com:2101/$ONOCOY_USERNAME#rtcm3 -msg "1006(10), 1033(10), 1077, 1087, 1097, 1107, 1117, 1127, 1137, 1230" $RTCM_MSG_COMMON $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -t 0 &
 fi
 
 # Run the third command only if all required parameters are specified
 if [ -n "$PORT_NUMBER" ]; then
-    str2str -in tcpcli://127.0.0.1:5015#rtcm3 -out tcpcli://ntrip.rtkdirect.com:$PORT_NUMBER#rtcm3 $RTCM_MSG_COMMON &
+    str2str -in tcpcli://127.0.0.1:5015#rtcm3 -out tcpcli://ntrip.rtkdirect.com:$PORT_NUMBER#rtcm3 -msg "1006(10), 1033(10), 1077, 1087, 1097, 1107, 1117, 1127, 1137, 1230" $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -t 0 &
 fi
 
 # Reset the 'exit immediately' option
