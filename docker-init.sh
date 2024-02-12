39e6ca793b0590b1d06d3f3f37240f93b1f3136f
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2024-02-11 21:40:07 -0600

    Update docker-init.sh

--- docker-init.sh
+++ docker-init.sh
@@ -35,12 +35,12 @@ fi
 
 # Run the second command only if all required parameters are specified
 if [ -n "$PASSWORD" ] && [ -n "$ONOCOY_USERNAME" ]; then
-    str2str -in tcpcli://127.0.0.1:5015#rtcm3 -out ntrips://:$PASSWORD@servers.onocoy.com:2101/$ONOCOY_USERNAME#rtcm3 -msg "1006(10), 1033(10), 1077, 1087, 1097, 1107, 1117, 1127, 1137, 1230" $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -t 0 &
+    str2str -in tcpcli://127.0.0.1:5015#rtcm3 -msg "1006(10), 1033(10), 1077, 1087, 1097, 1107, 1117, 1127, 1137, 1230" -out ntrips://:$PASSWORD@servers.onocoy.com:2101/$ONOCOY_USERNAME#rtcm3 -msg "1006(10), 1033(10), 1077, 1087, 1097, 1107, 1117, 1127, 1137, 1230" $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -t 0 &
 fi
 
 # Run the third command only if all required parameters are specified
 if [ -n "$PORT_NUMBER" ]; then
-    str2str -in tcpcli://127.0.0.1:5015#rtcm3 -out tcpcli://ntrip.rtkdirect.com:$PORT_NUMBER#rtcm3 -msg "1006(10), 1033(10), 1077, 1087, 1097, 1107, 1117, 1127, 1137, 1230" $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -t 0 &
+    str2str -in tcpcli://127.0.0.1:5015#rtcm3 -msg "1006(10), 1033(10), 1077, 1087, 1097, 1107, 1117, 1127, 1137, 1230" -out tcpcli://ntrip.rtkdirect.com:$PORT_NUMBER#rtcm3 -msg "1006(10), 1033(10), 1077, 1087, 1097, 1107, 1117, 1127, 1137, 1230" $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -t 0 &
 fi
 
 # Keep the script running indefinitely
