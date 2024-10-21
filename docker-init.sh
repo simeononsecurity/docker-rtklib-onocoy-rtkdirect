eee7dbbd7f44e8f9f8f03911f86e175e34e014e8
SimeonOnSecurity <4913771+simeononsecurity@users.noreply.github.com>
2024-10-21 03:01:45 -0500

    Update docker-init.sh

--- docker-init.sh
+++ docker-init.sh
@@ -7,7 +7,8 @@ export DATA_BITS="${DATA_BITS:-8}"
 export PARITY="${PARITY:-n}"
 export STOP_BITS="${STOP_BITS:-1}"
 export RTCM_MSGS="${RTCM_MSGS:-\"1005(30),1006(30),1007(30),1019,1033(30),1042,1044,1045,1046,1077(1),1087(1),1097(1),1107(1),1117(1),1127(1),1137(1),1230(1)\"}"
-export TCP_OUTPUT_PORT="${TCP_OUTPUT_PORT:-5015}"
+export RAW_TCP_OUTPUT_PORT="${TCP_OUTPUT_PORT:-5015}"
+export RTCM_TCP_OUTPUT_PORT="${TCP_OUTPUT_PORT:-5016}"
 export TCP_INPUT_PORT="${TCP_INPUT_PORT}"
 export TCP_INPUT_IP="${TCP_INPUT_IP}"
 export TCP_SERVER_SETUP_SUCCESSFUL="${TCP_SERVER_SETUP_SUCCESSFUL:-0}"
@@ -85,16 +86,16 @@ run_onocoy_server() {
                 if [ -n "$ONOCOY_MOUNTPOINT" ]; then
                     echo "Starting stunnel and NTRIP server with SSL and mountpoint."
                     stunnel /etc/stunnel/stunnel.conf &
-                    run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${TCP_OUTPUT_PORT}" -O 1 -a "127.0.0.1" -p "2101" -m "$ONOCOY_MOUNTPOINT" -n "$ONOCOY_USERNAME" -c "$ONOCOY_PASSWORD" -R 5 &
+                    run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${RTCM_TCP_OUTPUT_PORT}" -O 1 -a "127.0.0.1" -p "2101" -m "$ONOCOY_MOUNTPOINT" -n "$ONOCOY_USERNAME" -c "$ONOCOY_PASSWORD" -R 5 &
                 else
                     echo "Starting stunnel and NTRIP server with SSL and no mountpoint."
                     stunnel /etc/stunnel/stunnel.conf &
-                    run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${TCP_OUTPUT_PORT}" -O 3 -a "127.0.0.1" -p "2101" -m "$ONOCOY_USERNAME" -c "$ONOCOY_PASSWORD" -R 5 &
+                    run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${RTCM_TCP_OUTPUT_PORT}" -O 3 -a "127.0.0.1" -p "2101" -m "$ONOCOY_USERNAME" -c "$ONOCOY_PASSWORD" -R 5 &
                 fi
             else
                 echo "Starting stunnel and str2str with SSL."
                 stunnel /etc/stunnel/stunnel.conf &
-                run_and_retry str2str -in "tcpcli://127.0.0.1:${TCP_OUTPUT_PORT}#rtcm3" -out "ntrips://:${ONOCOY_PASSWORD}@127.0.0.1:2101/${ONOCOY_USERNAME}#rtcm3" -msg "$RTCM_MSGS" $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
+                run_and_retry str2str -in "tcpcli://127.0.0.1:${RTCM_TCP_OUTPUT_PORT}#rtcm3" -out "ntrips://:${ONOCOY_PASSWORD}@127.0.0.1:2101/${ONOCOY_USERNAME}#rtcm3" -msg "$RTCM_MSGS" $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
             fi
         else
             echo "ONOCOY_USE_SSL is disabled. Using non-secure connection."
@@ -102,14 +103,14 @@ run_onocoy_server() {
                 echo "ONOCOY_USE_NTRIPSERVER is enabled."
                 if [ -n "$ONOCOY_MOUNTPOINT" ]; then
                     echo "Starting NTRIP server without SSL and with mountpoint."
-                    run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${TCP_OUTPUT_PORT}" -O 1 -a "servers.onocoy.com" -p "2101" -m "$ONOCOY_MOUNTPOINT" -n "$ONOCOY_USERNAME" -c "$ONOCOY_PASSWORD" -R 5 &
+                    run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${RTCM_TCP_OUTPUT_PORT}" -O 1 -a "servers.onocoy.com" -p "2101" -m "$ONOCOY_MOUNTPOINT" -n "$ONOCOY_USERNAME" -c "$ONOCOY_PASSWORD" -R 5 &
                 else
                     echo "Starting NTRIP server without SSL and no mountpoint."
-                    run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${TCP_OUTPUT_PORT}" -O 3 -a "servers.onocoy.com" -p "2101" -m "$ONOCOY_USERNAME" -c "$ONOCOY_PASSWORD" -R 5 &
+                    run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${RTCM_TCP_OUTPUT_PORT}" -O 3 -a "servers.onocoy.com" -p "2101" -m "$ONOCOY_USERNAME" -c "$ONOCOY_PASSWORD" -R 5 &
                 fi
             else
                 echo "Starting str2str without SSL."
-                run_and_retry str2str -in "tcpcli://127.0.0.1:${TCP_OUTPUT_PORT}#rtcm3" -out "ntrips://:${ONOCOY_PASSWORD}@servers.onocoy.com:2101/${ONOCOY_USERNAME}#rtcm3" -msg "$RTCM_MSGS" $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
+                run_and_retry str2str -in "tcpcli://127.0.0.1:${RTCM_TCP_OUTPUT_PORT}#rtcm3" -out "ntrips://:${ONOCOY_PASSWORD}@servers.onocoy.com:2101/${ONOCOY_USERNAME}#rtcm3" -msg "$RTCM_MSGS" $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
             fi
         fi
     fi
@@ -124,10 +125,10 @@ run_rtkdirect_server() {
         echo "RTKDIRECT_MOUNTPOINT: $RTKDIRECT_MOUNTPOINT"
         if [ "$RTKDIRECT_USE_NTRIPSERVER" = true ]; then
             echo "Starting NTRIP server for RTKDirect."
-            run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${TCP_OUTPUT_PORT}" -O 1 -a "ntrip.rtkdirect.com" -p "2101" -m "$RTKDIRECT_MOUNTPOINT" -n "$RTKDIRECT_USERNAME" -c "$RTKDIRECT_PASSWORD" -R 5 &
+            run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${RTCM_TCP_OUTPUT_PORT}" -O 1 -a "ntrip.rtkdirect.com" -p "2101" -m "$RTKDIRECT_MOUNTPOINT" -n "$RTKDIRECT_USERNAME" -c "$RTKDIRECT_PASSWORD" -R 5 &
         else
             echo "Starting str2str for RTKDirect."
-            run_and_retry str2str -in "tcpcli://127.0.0.1:${TCP_OUTPUT_PORT}#rtcm3" -out "ntrips://${RTKDIRECT_USERNAME}:${RTKDIRECT_PASSWORD}@ntrip.rtkdirect.com:2101/${RTKDIRECT_MOUNTPOINT}#rtcm3" -msg "$RTCM_MSGS" $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
+            run_and_retry str2str -in "tcpcli://127.0.0.1:${RTCM_TCP_OUTPUT_PORT}#rtcm3" -out "ntrips://${RTKDIRECT_USERNAME}:${RTKDIRECT_PASSWORD}@ntrip.rtkdirect.com:2101/${RTKDIRECT_MOUNTPOINT}#rtcm3" -msg "$RTCM_MSGS" $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
         fi
     fi
 }
@@ -136,8 +137,9 @@ run_rtkdirect_server() {
 if [ -n "$SERIAL_INPUT" ]; then
     echo "### Starting RTKLIB SERIAL INPUT TCPSERVER ###"
     echo "SERIAL_INPUT: $SERIAL_INPUT"
-    echo "TCP_OUTPUT_PORT: $TCP_OUTPUT_PORT"
-    run_and_retry str2str -in "$SERIAL_INPUT#rtcm3" -out "tcpsvr://0.0.0.0:${TCP_OUTPUT_PORT}#rtcm3" -b 1 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
+    echo "RTCM_TCP_OUTPUT_PORT: $RTCM_TCP_OUTPUT_PORT"
+    run_and_retry str2str -in "$SERIAL_INPUT" -out "tcpsvr://0.0.0.0:${RAW_TCP_OUTPUT_PORT}" -b 1 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
+    run_and_retry str2str -in "tcpcli://127.0.0.1:${RAW_TCP_OUTPUT_PORT}#rtcm3" -out "tcpsvr://0.0.0.0:${RTCM_TCP_OUTPUT_PORT}#rtcm3" -msg "$RTCM_MSGS" -b 1 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
     TCP_SERVER_SETUP_SUCCESSFUL=1
 else
     echo "No Serial / USB Option Specified. Checking for TCP Input Options..."
@@ -145,9 +147,10 @@ else
         echo "TCP Input IP and Port are specified. Proceeding with TCP Input..."
         echo "TCP_INPUT_PORT: $TCP_INPUT_PORT"
         echo "TCP_INPUT_IP: $TCP_INPUT_IP"
-        echo "TCP_OUTPUT_PORT: $TCP_OUTPUT_PORT"
+        echo "RTCM_TCP_OUTPUT_PORT: $RTCM_TCP_OUTPUT_PORT"
         echo "### Starting RTKLIB TCP INPUT TCPSERVER ###"
-        run_and_retry str2str -in "tcpcli://${TCP_INPUT_IP}:${TCP_INPUT_PORT}" -out "tcpsvr://0.0.0.0:${TCP_OUTPUT_PORT}" -b 1 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
+        run_and_retry str2str -in "tcpcli://${TCP_INPUT_IP}:${TCP_INPUT_PORT}" -out "tcpsvr://0.0.0.0:${RAW_TCP_OUTPUT_PORT}" -b 1 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
+        run_and_retry str2str -in "tcpcli://127.0.0.1:${RAW_TCP_OUTPUT_PORT}#rtcm3" -out "tcpsvr://0.0.0.0:${RTCM_TCP_OUTPUT_PORT}#rtcm3" -msg "$RTCM_MSGS" -b 1 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
         TCP_SERVER_SETUP_SUCCESSFUL=1
     else
         echo "TCP Input IP or Port not specified. Please define TCP_INPUT_IP and TCP_INPUT_PORT."
