12d0ac2ffcf04e6e562bf0d0d816901c35dcd5c0
SimeonOnSecurity <4913771+simeononsecurity@users.noreply.github.com>
2024-10-21 01:53:18 -0500

    Increase Verbosity of Output Messages

--- docker-init.sh
+++ docker-init.sh
@@ -16,9 +16,19 @@ export ONOCOY_USE_NTRIPSERVER="${ONOCOY_USE_NTRIPSERVER:-false}"
 export RTKDIRECT_USE_NTRIPSERVER="${RTKDIRECT_USE_NTRIPSERVER:-true}"
 export RTKLIB_VERBOSITY="${RTKLIB_VERBOSITY:-1}"
 
-# Construct SERIAL_INPUT using individual components only if TCP input is not use as a source
+echo "### Setting Up SERIAL_INPUT Components ###"
+echo "USB_PORT: $USB_PORT"
+echo "BAUD_RATE: $BAUD_RATE"
+echo "DATA_BITS: $DATA_BITS"
+echo "PARITY: $PARITY"
+echo "STOP_BITS: $STOP_BITS"
+
+# Construct SERIAL_INPUT using individual components only if TCP input is not used as a source
 if [ -z "$TCP_INPUT_PORT" ] && [ -z "$TCP_INPUT_IP" ]; then
     export SERIAL_INPUT="serial://${USB_PORT}:${BAUD_RATE}:${DATA_BITS}:${PARITY}:${STOP_BITS}"
+    echo "SERIAL_INPUT set to: $SERIAL_INPUT"
+else
+    echo "TCP input specified. SERIAL_INPUT will not be used."
 fi
 
 # Exit immediately if a command fails
@@ -36,71 +46,87 @@ run_and_retry() {
     done
 }
 
-
 # Check if LAT, LONG, and ELEVATION are specified
 if [ -n "$LAT" ] && [ -n "$LONG" ] && [ -n "$ELEVATION" ]; then
     LAT_LONG_ELEVATION="-p \"$LAT $LONG $ELEVATION\""
-    echo "LAT LOG ELEVATION: $LAT_LONG_ELEVATION"
+    echo "LAT, LONG, and ELEVATION specified: $LAT_LONG_ELEVATION"
+else
+    echo "LAT, LONG, or ELEVATION not specified. Skipping related options."
 fi
 
 # Check if INSTRUMENT is specified
 if [ -n "$INSTRUMENT" ]; then
     INSTRUMENT="-i \"$INSTRUMENT\""
-    echo "INSTRUMENT: $INSTRUMENT"
+    echo "INSTRUMENT specified: $INSTRUMENT"
+else
+    echo "INSTRUMENT not specified. Skipping related options."
 fi
 
 # Check if ANTENNA is specified
 if [ -n "$ANTENNA" ]; then
     ANTENNA="-a \"$ANTENNA\""
-    echo "ANTENNA: $ANTENNA"
+    echo "ANTENNA specified: $ANTENNA"
+else
+    echo "ANTENNA not specified. Skipping related options."
 fi
 
 # Function for running the third command
 run_onocoy_server() {
-    if [ -n "$ONOCOY_PASSWORD" ] && [ -n "$ONOCOY_USERNAME" ] ; then
+    if [ -n "$ONOCOY_PASSWORD" ] && [ -n "$ONOCOY_USERNAME" ]; then
+        echo "### Starting ONOCOY Server Setup ###"
         echo "ONOCOY_PASSWORD: $ONOCOY_PASSWORD"
         echo "ONOCOY_USERNAME: $ONOCOY_USERNAME"
         echo "ONOCOY_MOUNTPOINT: $ONOCOY_MOUNTPOINT"
+        
         if [ "$ONOCOY_USE_SSL" = true ]; then
+            echo "ONOCOY_USE_SSL is enabled. Using secure connection."
             if [ "$ONOCOY_USE_NTRIPSERVER" = true ]; then
+                echo "ONOCOY_USE_NTRIPSERVER is enabled."
                 if [ -n "$ONOCOY_MOUNTPOINT" ]; then
+                    echo "Starting stunnel and NTRIP server with SSL and mountpoint."
                     stunnel /etc/stunnel/stunnel.conf &
                     run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${TCP_OUTPUT_PORT}" -O 1 -a "127.0.0.1" -p "2101" -m "$ONOCOY_MOUNTPOINT" -n "$ONOCOY_USERNAME" -c "$ONOCOY_PASSWORD" -R 5 &
                 else
+                    echo "Starting stunnel and NTRIP server with SSL and no mountpoint."
                     stunnel /etc/stunnel/stunnel.conf &
                     run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${TCP_OUTPUT_PORT}" -O 3 -a "127.0.0.1" -p "2101" -m "$ONOCOY_USERNAME" -n "" -c "$ONOCOY_PASSWORD" -R 5 &
                 fi
             else
+                echo "Starting stunnel and str2str with SSL."
                 stunnel /etc/stunnel/stunnel.conf &
                 run_and_retry str2str -in "tcpcli://127.0.0.1:${TCP_OUTPUT_PORT}#rtcm3" -out "ntrips://:${ONOCOY_PASSWORD}@127.0.0.1:2101/${ONOCOY_USERNAME}#rtcm3" -msg "$RTCM_MSGS" $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
             fi
         else
+            echo "ONOCOY_USE_SSL is disabled. Using non-secure connection."
             if [ "$ONOCOY_USE_NTRIPSERVER" = true ]; then
-                if [ -n "$ONOCOY_MOUNTPOINT"  ]; then
+                echo "ONOCOY_USE_NTRIPSERVER is enabled."
+                if [ -n "$ONOCOY_MOUNTPOINT" ]; then
+                    echo "Starting NTRIP server without SSL and with mountpoint."
                     run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${TCP_OUTPUT_PORT}" -O 1 -a "servers.onocoy.com" -p "2101" -m "$ONOCOY_MOUNTPOINT" -n "$ONOCOY_USERNAME" -c "$ONOCOY_PASSWORD" -R 5 &
                 else
+                    echo "Starting NTRIP server without SSL and no mountpoint."
                     run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${TCP_OUTPUT_PORT}" -O 3 -a "servers.onocoy.com" -p "2101" -m "$ONOCOY_MOUNTPOINT" -n "" -c "$ONOCOY_PASSWORD" -R 5 &
                 fi
             else
+                echo "Starting str2str without SSL."
                 run_and_retry str2str -in "tcpcli://127.0.0.1:${TCP_OUTPUT_PORT}#rtcm3" -out "ntrips://:${ONOCOY_PASSWORD}@servers.onocoy.com:2101/${ONOCOY_USERNAME}#rtcm3" -msg "$RTCM_MSGS" $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
             fi
         fi
     fi
 }
 
-
-# Function for running the third command
+# Function for running the RTKDirect command
 run_rtkdirect_server() {
     if [ -n "$RTKDIRECT_PASSWORD" ] && [ -n "$RTKDIRECT_USERNAME" ] && [ -n "$RTKDIRECT_MOUNTPOINT" ]; then
+        echo "### Starting RTKDirect Server Setup ###"
         echo "RTKDIRECT_PASSWORD: $RTKDIRECT_PASSWORD"
         echo "RTKDIRECT_USERNAME: $RTKDIRECT_USERNAME"
         echo "RTKDIRECT_MOUNTPOINT: $RTKDIRECT_MOUNTPOINT"
         if [ "$RTKDIRECT_USE_NTRIPSERVER" = true ]; then
-            sleep 1
-            echo "STARTING NTRIPSERVER RTKDIRECT NTRIPv2 SERVER...."
+            echo "Starting NTRIP server for RTKDirect."
             run_and_retry ntripserver -M 2 -H "127.0.0.1" -P "${TCP_OUTPUT_PORT}" -O 1 -a "ntrip.rtkdirect.com" -p "2101" -m "$RTKDIRECT_MOUNTPOINT" -n "$RTKDIRECT_USERNAME" -c "$RTKDIRECT_PASSWORD" -R 5 &
         else
-            echo "STARTING RTKLIB RTKDIRECT NTRIPv1 SERVER...."
+            echo "Starting str2str for RTKDirect."
             run_and_retry str2str -in "tcpcli://127.0.0.1:${TCP_OUTPUT_PORT}#rtcm3" -out "ntrips://${RTKDIRECT_USERNAME}:${RTKDIRECT_PASSWORD}@ntrip.rtkdirect.com:2101/${RTKDIRECT_MOUNTPOINT}#rtcm3" -msg "$RTCM_MSGS" $LAT_LONG_ELEVATION $INSTRUMENT $ANTENNA -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
         fi
     fi
@@ -108,19 +134,19 @@ run_rtkdirect_server() {
 
 # Run the first command only if all required parameters are specified
 if [ -n "$SERIAL_INPUT" ]; then
-    echo "SERIAL_INPUT is \"$SERIAL_INPUT\""
-    echo "TCP_OUTPUT_PORT is \"$TCP_OUTPUT_PORT\""
-    echo "STARTING RTKLIB SERIAL INPUT TCPSERVER...."
+    echo "### Starting RTKLIB SERIAL INPUT TCPSERVER ###"
+    echo "SERIAL_INPUT: $SERIAL_INPUT"
+    echo "TCP_OUTPUT_PORT: $TCP_OUTPUT_PORT"
     run_and_retry str2str -in "$SERIAL_INPUT" -out "tcpsvr://0.0.0.0:${TCP_OUTPUT_PORT}" -b 1 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
     TCP_SERVER_SETUP_SUCCESSFUL=1
 else
-    echo "No Serial / USB Option Specified, Checking for TCP Input Options..."
+    echo "No Serial / USB Option Specified. Checking for TCP Input Options..."
     if [ -n "$TCP_INPUT_PORT" ] && [ -n "$TCP_INPUT_IP" ]; then
-        echo "TCP Input IP and Port are specified. Processing with TCP Input..."
-        echo "TCP_INPUT_PORT is \"$TCP_INPUT_PORT\""
-        echo "TCP_INPUT_IP is \"$TCP_INPUT_IP\""
-        echo "TCP_OUTPUT_PORT is \"$TCP_OUTPUT_PORT\""
-        echo "STARTING RTKLIB TCP INPUT TCPSERVER...."
+        echo "TCP Input IP and Port are specified. Proceeding with TCP Input..."
+        echo "TCP_INPUT_PORT: $TCP_INPUT_PORT"
+        echo "TCP_INPUT_IP: $TCP_INPUT_IP"
+        echo "TCP_OUTPUT_PORT: $TCP_OUTPUT_PORT"
+        echo "### Starting RTKLIB TCP INPUT TCPSERVER ###"
         run_and_retry str2str -in "tcpcli://${TCP_INPUT_IP}:${TCP_INPUT_PORT}" -out "tcpsvr://0.0.0.0:${TCP_OUTPUT_PORT}" -b 1 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
         TCP_SERVER_SETUP_SUCCESSFUL=1
     else
@@ -131,7 +157,7 @@ fi
 
 # Call functions based on the success of the TCP server setup
 if [ "$TCP_SERVER_SETUP_SUCCESSFUL" -eq 1 ]; then
-    echo "TCP server setup successful. Running command blocks..."
+    echo "TCP server setup successful. Running additional command blocks..."
     run_onocoy_server
     run_rtkdirect_server
 else
@@ -146,4 +172,3 @@ done
 
 # Reset the 'exit immediately' option
 set +e
-
