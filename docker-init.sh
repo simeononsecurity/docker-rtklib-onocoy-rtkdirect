cb5313b2e66d205be20e8600172ba3744a89c8a9
SimeonOnSecurity <4913771+simeononsecurity@users.noreply.github.com>
2024-10-21 22:48:22 -0500

    Update docker-init.sh

--- docker-init.sh
+++ docker-init.sh
@@ -6,7 +6,7 @@ export BAUD_RATE="${BAUD_RATE:-115200}"
 export DATA_BITS="${DATA_BITS:-8}"
 export PARITY="${PARITY:-n}"
 export STOP_BITS="${STOP_BITS:-1}"
-export RTCM_MSGS="${RTCM_MSGS:-\"1005(30),1006(30),1007(30),1019,1033(30),1042,1044,1045,1046,1077(1),1087(1),1097(1),1107(1),1117(1),1127(1),1137(1),1230(1)\"}"
+export RTCM_MSGS="${RTCM_MSGS:-\"1077(1),1087(1),1097(1),1107(1),1117(1),1127(1),1137(1),1230(1)\"}"
 export RAW_TCP_OUTPUT_PORT="${TCP_OUTPUT_PORT:-5015}"
 export RTCM_TCP_OUTPUT_PORT="${RTCM_TCP_OUTPUT_PORT:-5016}"
 export TCP_INPUT_PORT="${TCP_INPUT_PORT}"
@@ -139,7 +139,7 @@ if [ -n "$SERIAL_INPUT" ]; then
     echo "SERIAL_INPUT: $SERIAL_INPUT"
     echo "RTCM_TCP_OUTPUT_PORT: $RTCM_TCP_OUTPUT_PORT"
     run_and_retry str2str -in "$SERIAL_INPUT" -out "tcpsvr://0.0.0.0:${RAW_TCP_OUTPUT_PORT}" -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
-    run_and_retry str2str -in "tcpcli://127.0.0.1:${RAW_TCP_OUTPUT_PORT}#rtcm3" -out "tcpsvr://0.0.0.0:${RTCM_TCP_OUTPUT_PORT}#rtcm3" -msg "$RTCM_MSGS" -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
+    run_and_retry str2str -in "tcpcli://127.0.0.1:${RAW_TCP_OUTPUT_PORT}#rtcm3" -out "tcpsvr://0.0.0.0:${RTCM_TCP_OUTPUT_PORT}#rtcm3" -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
     TCP_SERVER_SETUP_SUCCESSFUL=1
 else
     echo "No Serial / USB Option Specified. Checking for TCP Input Options..."
@@ -150,7 +150,7 @@ else
         echo "RTCM_TCP_OUTPUT_PORT: $RTCM_TCP_OUTPUT_PORT"
         echo "### Starting RTKLIB TCP INPUT TCPSERVER ###"
         run_and_retry str2str -in "tcpcli://${TCP_INPUT_IP}:${TCP_INPUT_PORT}" -out "tcpsvr://0.0.0.0:${RAW_TCP_OUTPUT_PORT}" -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
-        run_and_retry str2str -in "tcpcli://127.0.0.1:${RAW_TCP_OUTPUT_PORT}#rtcm3" -out "tcpsvr://0.0.0.0:${RTCM_TCP_OUTPUT_PORT}#rtcm3" -msg "$RTCM_MSGS" -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
+        run_and_retry str2str -in "tcpcli://127.0.0.1:${RAW_TCP_OUTPUT_PORT}#rtcm3" -out "tcpsvr://0.0.0.0:${RTCM_TCP_OUTPUT_PORT}#rtcm3" -b 0 -t $RTKLIB_VERBOSITY -s 30000 -r 30000 -n 1 &
         TCP_SERVER_SETUP_SUCCESSFUL=1
     else
         echo "TCP Input IP or Port not specified. Please define TCP_INPUT_IP and TCP_INPUT_PORT."
